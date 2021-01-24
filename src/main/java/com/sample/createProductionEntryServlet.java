f33afd5ae7951dfc60ebbbb19ee7e0dc2095ad65
RatkathVamsi <kskvamsi@gmail.com>
2021-01-24 23:19:27 +0530

    Navigated through webpages and from submit

--- src/main/java/com/sample/createProductionEntryServlet.java
+++ src/main/java/com/sample/createProductionEntryServlet.java
@@ -8,16 +8,34 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
 import java.io.IOException;
 import java.util.List;
 
 
+
 @WebServlet(
-        name = "selectliquorservlet",
-        urlPatterns = "/SelectLiquor"
+        name = "createProductionEntryservlet",
+        urlPatterns = "/createProductionEntry"
 )
-public class SelectLiquorServlet extends HttpServlet {
+public class createProductionEntryServlet extends HttpServlet {
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        String consumptionQties[] = req.getParameterValues("consumptionQty");
+               // getParameter("consumptionQty");
+        for(int i=0;i< consumptionQties.length;i++)
+        System.out.println("Liquor Type= " + consumptionQties[i]);
+       // LiquorService liquorService = new LiquorService();
+       // LiquorType l = LiquorType.valueOf(liquorType);
+    }
+}
+
+
+
+
+
+/*public class SelectLiquorServlet extends HttpServlet {
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
@@ -35,3 +53,5 @@ public class SelectLiquorServlet extends HttpServlet {
 
     }
 }
+*/
+
